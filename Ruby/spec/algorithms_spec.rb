9131fbefcd9a141edbdd0d002abaa7f781808c7d
David Runger <daverunger@gmail.com>
2015-12-02 02:32:46 -0800

    minor correction to "silly years" specs
    The problem statement specifies that the ten *subsequent* silly years
    should be returned. In other words, the initial year that is passed to
    the "silly years" function should not be included in the result.

--- Ruby/spec/algorithms_spec.rb
+++ Ruby/spec/algorithms_spec.rb
@@ -109,12 +109,12 @@ end
 
 describe 'silly_years' do
   it 'should return the ten subsequent silly years' do
-    array = [1978, 2307, 2417, 2527, 2637, 2747, 2857, 2967, 3406, 3516]
+    array = [2307, 2417, 2527, 2637, 2747, 2857, 2967, 3406, 3516, 3626]
     expect(silly_years(1978)).to match_array(array)
   end
 
   it 'should return the ten subsequent silly years' do
-    array = [2307, 2417, 2527, 2637, 2747, 2857, 2967, 3406, 3516, 3626]
+    array = [2417, 2527, 2637, 2747, 2857, 2967, 3406, 3516, 3626, 3736]
     expect(silly_years(2307)).to match_array(array)
   end
 end
