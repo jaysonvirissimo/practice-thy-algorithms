f610a146845bcb53f5736c36e3ddfb588cc50c41
Jayson Virissimo <jayson.virissimo@asu.edu>
2016-03-09 19:51:07 -0700

    Enforce recursion for recursive sum

--- Ruby/spec/algorithms_spec.rb
+++ Ruby/spec/algorithms_spec.rb
@@ -20,6 +20,11 @@ end
 describe 'sum_rec' do
   specify { expect(sum_rec([1, 2, 3])).to eq(6) }
   specify { expect(sum_rec([99, 66, 33])).to eq(198) }
+
+  it 'calls itself' do
+    expect(self).to receive(:sum_rec).at_least(:twice).and_call_original
+    sum_rec([10, 20, 30])
+  end
 end
 
 describe 'fibs' do
