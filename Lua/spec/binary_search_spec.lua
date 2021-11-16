f74f0b2b8ab67f642d862e9db351becef362dc04
Jayson Virissimo <jayson.virissimo@asu.edu>
2021-11-15 22:04:18 -0700

    Add another Lua expectation

--- Lua/spec/binary_search_spec.lua
+++ Lua/spec/binary_search_spec.lua
@@ -1,6 +1,12 @@
 require("../src/binary_search")
 
 describe("#binary_search", function()
+  it("should return the index of the found element", function()
+    local array = {1, 5, 13, 23, 305, 333, 402, 454, 500}
+
+    assert.is_equal(binary_search(array, 13), 3)
+  end)
+
   it("should return nil if the target isn't found", function()
     local array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
     local result = binary_search(array, 11)
