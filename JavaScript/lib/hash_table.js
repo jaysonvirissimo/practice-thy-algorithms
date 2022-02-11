37a7dc02fe54d6a5460a937e8af35e4d94060ab4
Jayson Virissimo <jayson.virissimo@asu.edu>
2022-02-10 17:27:54 -0700

    Refactor hash table

--- JavaScript/lib/hash_table.js
+++ JavaScript/lib/hash_table.js
@@ -1,9 +1,15 @@
 // Implement a hash table (also called a hash map).
+// Compute hash codes using the given lose lose hash function.
 class HashTable {
-    constuctor() {
+    constructor() {
       this.table = [];
     }
     get() {}
+    loseloseHashCode(key) {
+      let hash = 0;
+      for (let i = 0; i < key.length; i++) { hash += key.charCodeAt(i); }
+      return hash % 37;
+    }
     put() {}
     remove() {}
 }
--- JavaScript/test/hash_table.test.js
+++ JavaScript/test/hash_table.test.js
@@ -1,5 +1,15 @@
 const HashTable = require('../lib/hash_table');
 
+test("is implemented using a JavaScript array", () => {
+  const hashTable = new HashTable();
+  expect(Array.isArray(hashTable.table)).toBe(true);
+});
+
+test("preserves the given lose lose hash function", () => {
+  const hashTable = new HashTable();
+  expect(hashTable.loseloseHashCode("foo")).toBe(28);
+});
+
 test("get", () => {
   let hashTable = new HashTable();
   hashTable.put("foo", "bar");
