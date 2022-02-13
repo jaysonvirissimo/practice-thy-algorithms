dd2233b368292d3880f57fda6540db56278f1bfe
Jayson Virissimo <jayson.virissimo@asu.edu>
2022-02-12 22:30:58 -0700

    Increase in words test coverage

--- Ruby/spec/in_words_spec.rb
+++ Ruby/spec/in_words_spec.rb
@@ -1,5 +1,9 @@
 require "in_words"
 
 describe "in_words" do
-  it { expect(in_words(132)).to eq("one hundred and thirty two") }
+  it { expect(in_words(0)).to eq("zero") }
+  it { expect(in_words(11)).to eq("eleven") }
+  it { expect(in_words(23)).to eq("twenty three") }
+  it { expect(in_words(134)).to eq("one hundred thirty four") }
+  it { expect(in_words(1456)).to eq("one thousand four hundred fifty six") }
 end
