5acb25c28112839377042acc37598151217d826f
David Runger <davidjrunger@gmail.com>
2022-11-30 23:51:36 -0600

    Reference iterative_factorial rather than recursive_factorial in spec
    Also, fix the RSpec expectation for the number of method calls.

--- Ruby/spec/iterative_factorial_spec.rb
+++ Ruby/spec/iterative_factorial_spec.rb
@@ -1,14 +1,15 @@
 require "iterative_factorial"
 
 describe "iterative_factorial" do
-  specify { expect(recursive_factorial(0)).to eq(1) }
-  specify { expect(recursive_factorial(1)).to eq(1) }
-  specify { expect(recursive_factorial(3)).to eq(6) }
-  specify { expect(recursive_factorial(5)).to eq(120) }
+  specify { expect(iterative_factorial(0)).to eq(1) }
+  specify { expect(iterative_factorial(1)).to eq(1) }
+  specify { expect(iterative_factorial(3)).to eq(6) }
+  specify { expect(iterative_factorial(5)).to eq(120) }
 
   it "does not call itself" do
-    expect(self).to_not receive(:iterative_factorial)
-      .at_least(:twice).and_call_original
-    recursive_factorial(10)
+    allow(self).to receive(:iterative_factorial).at_least(:once).and_call_original
+    iterative_factorial(10)
+
+    expect(self).to have_received(:iterative_factorial).exactly(:once)
   end
 end
