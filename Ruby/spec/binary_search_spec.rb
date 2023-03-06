55ec4143541a4cc45b93a708a5a8ffe5fac8b715
Jayson Virissimo <jayson.virissimo@asu.edu>
2023-03-06 08:41:42 -0700

    Binary search must beat linear search

--- Ruby/spec/binary_search_spec.rb
+++ Ruby/spec/binary_search_spec.rb
@@ -1,7 +1,8 @@
+require "spec_helper"
 require "binary_search"
 
 describe "binary_search" do
-  it "([1, 5, 13, 23, 305, 333, 402, 454, 500], 13) should return 2" do
+  it "returns the proper index" do
     expect(binary_search([1, 5, 13, 23, 305, 333, 402, 454, 500], 13)).to eq(2)
   end
 
@@ -9,5 +10,20 @@ describe "binary_search" do
     array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     target = 11
     expect(binary_search(array, target)).to be_nil
+  end
+
+  it "performs faster than linear search" do
+    array = (0..100_000).to_a
+    target = 64_000
+
+    expect { binary_search(array, target) }.to perform_faster_than { linear_search(array, target) }
+  end
+
+  def linear_search(array, target)
+    (0...array.length).each do |index|
+      return index if array[index] == target
+    end
+
+    nil
   end
 end
