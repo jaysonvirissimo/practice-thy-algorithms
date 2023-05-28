de3a83956b126a74f074c173caeb21b4bef01500
Jayson Virissimo <jayson.virissimo@asu.edu>
2023-05-28 13:18:44 -0700

    Don't require particular order for Python subsets

--- Python/test/test_subsets.py
+++ Python/test/test_subsets.py
@@ -3,8 +3,11 @@ from lib.subsets import subsets
 
 class TestSubsets(unittest.TestCase):
     def test_subsets(self):
-        self.assertEqual(subsets([]), [[]])
-        self.assertEqual(subsets([1, 2, 3]), [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]])
+        self.assertEqual(sorted(map(tuple, subsets([]))), sorted(map(tuple, [[]])))
+        self.assertEqual(
+            sorted(map(tuple, subsets([1, 2, 3]))),
+            sorted(map(tuple, [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]))
+        )
 
 if __name__ == '__main__':
     unittest.main()
